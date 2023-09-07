c8a6d4fc38aff8ce84035222c7d4e37fcd580dbc
Nadia <nadiahosisy@gmail.com>
2023-09-07 13:46:27 +0300

    fetch data from url

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const url = "https://course-api.com/react-tabs-project";
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
+
+  const fechJobs = async () => {};
   useEffect(() => {}, []);
   return <h2>Tabs Starter</h2>;
 };
