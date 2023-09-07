df12c031ef3c0b19b0790671aa33397fc2682900
Nadia <nadiahosisy@gmail.com>
2023-09-07 15:23:30 +0300

    add JobInfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import JobInfo from "./JobInfo";
 
 const url = "https://course-api.com/react-tabs-project";
 
@@ -23,6 +24,10 @@ const App = () => {
     );
   }
 
-  return <h2>Tabs Starter</h2>;
+  return (
+    <section className="jobs-center">
+      <JobInfo Jobs={jobs} />
+    </section>
+  );
 };
 export default App;
