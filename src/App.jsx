185fc5fc4675692d128d4b0626a8bc29efcc61a8
Nadia <nadiahosisy@gmail.com>
2023-09-07 13:45:02 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {}, []);
+  return <h2>Tabs Starter</h2>;
+};
+export default App;
